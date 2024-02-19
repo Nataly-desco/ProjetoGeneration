06918a5d576363628a37a9109c0bf15d7a1f690c
Nataly <natalycarvalho404@gmail.com>
2024-02-19 18:00:22 -0300

    first commit

---
+++ src/main/java/com/generation/ProjetoGeneration/ProjetoGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.generation.ProjetoGeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoGenerationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/ProjetoGeneration/ProjetoGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.ProjetoGeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
